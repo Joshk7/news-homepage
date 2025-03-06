6af033324b6af61eeb85ccb84a5ad8d6f2a52795
JoshK7 <joshk7683@gmail.com>
2025-03-05 21:15:30 -0600

    adding aria labels

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const dialog = document.getElementById("dialog");
 const dialogLinks = dialog.querySelectorAll("a");
 
 const handleOpen = (e) => {
+    openMenu.ariaExpanded = true;
     dialog.showModal();
 }
 
 const handleClose = (e) => {
+    openMenu.ariaExpanded = false;
     dialog.close();
 }
 
 const handleLink = (e) => {
+    openMenu.ariaExpanded = false;
     dialog.close();
 }
 
